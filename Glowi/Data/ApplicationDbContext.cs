95d13ea274e80c8eab70b032574d1d8771ec39a3
luisa <luisach2003@gmail.com>
2026-09-24 21:58:32 -0300

    Add Identity e primeira migration (tabelas do Identity) com ref ao banco do Supabase

--- Glowi/Data/ApplicationDbContext.cs
+++ Glowi/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Glowi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Glowi.Data
--- Glowi/Models/ApplicationUser.cs
+++ Glowi/Models/ApplicationUser.cs
@@ -1,6 +1,8 @@
-﻿namespace Glowi.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Glowi.Models
 {
-    public class ApplicationUser :IdentityUser
+    public class ApplicationUser : IdentityUser
     {
         public string Nome { get; set; } = string.Empty;
     }
